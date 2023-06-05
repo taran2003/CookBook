de9c0a2ce2a4149aae3d524de486605e043fccb2
taran2003 <taran2003@bk.ru>
2023-06-05 15:50:21 +0300

    Initial commit

---
+++ CookBoock/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CookBoock;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CookBoock/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CookBoock;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
